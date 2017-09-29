9773c3f34608aba1f5f67618f96e855dc421d275
Adam Weaver <parkersalterego92@gmail.com>
2017-09-28 18:10:07 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports:[
+        BrowserModule
+    ],
+    declarations: [
+        AppComponent
+      ],
+      bootstrap: [
+          AppComponent
+      ]
 })
-export class AppModule { }
+export class AppModule {}
